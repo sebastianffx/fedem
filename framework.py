3282f0eecf815bb5850ee068379072a9c40def9f
Antoine Madrona <antoine.madrona@epfl.ch>
2022-03-15 15:49:18 +0100

    OOP refactoring

---
+++ framework.py
@@ -0,0 +1,257 @@
+import copy
+import torch
+import random
+import monai
+import numpy as np
+
+from network import UNet_custom
+from monai.metrics import DiceMetric
+from torch.optim import Optimizer, Adam
+from torch.utils.tensorboard import SummaryWriter
+from weighting_schemes import average_weights, average_weights_beta, average_weights_softmax
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+dice_metric = DiceMetric(include_background=False, reduction="mean", get_not_nans=False)
+print(device)
+
+class Fedem:
+    def __init__(self, options):
+        #save all clients dataloader
+        self.dataloaders = options['dataloader']
+        self.writer = SummaryWriter(f"runs/llr{options['l_lr']}_glr{options['g_lr']}_le{options['l_epoch']}_ge{options['g_epoch']}_3sites")
+
+    def train_server(self, global_epoch, local_epoch, global_lr, local_lr):
+        for epoch in range(global_epoch):
+
+            #skiping center 2 as only 1 scan is available (train_loader sorted)
+            index=[0,1,2]
+            #perform sampling here if desired
+
+            # dispatch
+            self.dispatch(index)
+            # local updating
+            self.client_update(index, local_epoch, local_lr, epoch)
+            # aggregation
+            self.aggregation(index, global_lr)
+
+        return self.nn
+
+    def client_update(self, index, local_epoch, local_lr):
+        for k in index:
+            self.nns[k], round_loss = self.train(self.nns[k], k, self.dataloaders[k][0], local_epoch, local_lr)
+            
+            print(self.nns[k].name+" loss:", round_loss)
+            self.writer.add_scalar('training loss '+self.nns[k].name, round_loss, epoch)
+
+    def dispatch(self, index):
+        for j in index:
+            for old_params, new_params in zip(self.nns[j].parameters(), self.nn.parameters()):
+                old_params.data = new_params.data.clone()
+
+    def test(self, dataloader_test):
+        model=self.nn
+        model.eval()
+        pred = []
+        y = []
+        for test_data in dataloader_test:
+            with torch.no_grad():
+                test_img, test_label = test_data[0][:,:,:,:,0].to(device), test_data[1][:,:,:,:,0].to(device)
+                test_pred = model(test_img)
+                #what is the purpose of the line below?
+                test_pred =  test_pred>0.5 #This assumes one slice in the last dim
+                dice_metric(y_pred=test_pred, y=test_label)
+
+        # aggregate the final mean dice result
+        metric = dice_metric.aggregate().item()
+        dice_metric.reset()
+        print('dice:', metric)
+
+    def aggregation():
+        raise NotImplementedError
+
+    def train():
+        raise NotImplementedError
+
+class FedAvg(Fedem):
+    def __init__(self, options):
+        super(FedAvg, self).__init__(options)
+
+        self.weighting_scheme = options['weighting_scheme']
+
+        if options['weighting_scheme'] == 'BETA':
+            self.beta_val = options['beta_val']
+            #extract length of trianing loader for each site
+            self.trainloaders_lengths = [len(ldtr[0]) for ldtr in self.dataloaders]
+        elif options['weighting_scheme'] == 'SOFTMAX':
+            #extract length of trianing loader for each site
+            self.trainloaders_lengths = [len(ldtr[0]) for ldtr in self.dataloaders]
+        
+        #server model
+        self.nn = UNet_custom(spatial_dims=2,
+                              in_channels=1,
+                              out_channels=1,
+                              channels=(16, 32, 64, 128),
+                              strides=(2, 2, 2),
+                              kernel_size = (3,3),
+                              num_res_units=2,
+                              name='server',
+                              scaff=False).to(device)
+        
+        #create clients
+        self.nns = []
+        for i in range(len(options['clients'])):
+            temp = copy.deepcopy(self.nn)
+            temp.name = options['clients'][i]
+            self.nns.append(temp)
+
+    def train(self, ann, k, dataloader_train, local_epoch, local_lr):
+        #train client to train mode
+        ann.train()
+        ann.len = len(dataloader_train)
+                
+        loss_function = monai.losses.DiceLoss(sigmoid=True, include_background=False)
+        optimizer = Adam(ann.parameters(), local_lr)
+
+        for epoch in range(local_epoch):
+            for batch_data in dataloader_train:
+                inputs, labels = batch_data[0][:,:,:,:,0].to(device), batch_data[1][:,:,:,:,0].to(device)
+                y_pred = ann(inputs)
+                loss = loss_function(y_pred, labels)
+                optimizer.zero_grad()        
+                loss.backward()
+                optimizer.step()
+                            
+        return ann, loss.item()
+
+    def aggregation(self, index, global_lr):
+        client_weights=[]
+
+        #would be possible to use sampling here
+        for client_nn in self.nns:
+            client_weights.append(copy.deepcopy(client_nn.state_dict()))
+
+        #Agregating the weights with the selected weighting scheme
+        if self.weighting_scheme =='FEDAVG':
+            global_weights = average_weights(client_weights)
+        if self.weighting_scheme =='BETA':
+            sub_trainloaders_lengths = [self.trainloaders_lengths[idx] for idx in index]
+            global_weights = average_weights_beta(client_weights,sub_trainloaders_lengths,self.beta_val)
+        if self.weighting_scheme =='SOFTMAX':
+            sub_trainloaders_lengths = [self.trainloaders_lengths[idx] for idx in index]
+            global_weights = average_weights_softmax(client_weights,sub_trainloaders_lengths)
+
+        # Update global weights with the averaged model weights.
+        self.nn.load_state_dict(global_weights)
+
+class Scaffold(Fedem):
+    def __init__(self, options):
+        super(Scaffold, self).__init__(options)
+
+        self.K = options['K']
+        
+        #server model
+        self.nn = UNet_custom(spatial_dims=2,
+                             in_channels=1,
+                             out_channels=1,
+                             channels=(16, 32, 64, 128),
+                             strides=(2, 2, 2),
+                             kernel_size = (3,3),
+                             num_res_units=2,
+                             name='server',
+                             scaff=True).to(device)
+        
+        #control variables
+        for k, v in self.nn.named_parameters():
+            self.nn.control[k] = torch.zeros_like(v.data)
+            self.nn.delta_control[k] = torch.zeros_like(v.data)
+            self.nn.delta_y[k] = torch.zeros_like(v.data)
+        
+        #clients
+        self.nns = []
+        for i in range(len(options['clients'])):
+            temp = copy.deepcopy(self.nn)
+            temp.name = options['clients'][i]
+            temp.control       = copy.deepcopy(self.nn.control)  # ci
+            temp.delta_control = copy.deepcopy(self.nn.delta_control)  # ci
+            temp.delta_y       = copy.deepcopy(self.nn.delta_y)
+            self.nns.append(temp)
+
+    def aggregation(self, index, global_lr, **kwargs):
+        s = 0.0
+        for j in index:
+            # normal
+            s += self.nns[j].len
+        # compute
+        x = {}
+        c = {}
+        # init
+        for k, v in self.nns[0].named_parameters():
+            x[k] = torch.zeros_like(v.data)
+            c[k] = torch.zeros_like(v.data)
+
+        for j in index:
+            for k, v in self.nns[j].named_parameters():
+                x[k] += self.nns[j].delta_y[k] / len(index)  # averaging
+                c[k] += self.nns[j].delta_control[k] / len(index)  # averaging
+
+        # update x and c
+        for k, v in self.nn.named_parameters():
+            v.data += x[k].data*global_lr
+            self.nn.control[k].data += c[k].data * (len(index) / self.K)
+
+    def global_test(self, aggreg_dataloader_test):
+        model = self.nn
+        model.eval()
+        
+        #test the global model on each individual dataloader
+        for k, client in enumerate(self.nns):
+            print("testing on", client.name, "dataloader")
+            test(model, self.dataloaders[k][2])
+        
+        #test the global model on aggregated dataloaders
+        print("testing on all the data")
+        test(model, aggreg_dataloader_test)
+
+    def train(self, ann, idx, dataloader_train, local_epoch, local_lr):
+        #train client to train mode
+        ann.train()
+        ann.len = len(dataloader_train)
+                
+        x = copy.deepcopy(ann)
+        loss_function = monai.losses.DiceLoss(sigmoid=True,include_background=False)
+        optimizer = ScaffoldOptimizer(ann.parameters(), lr=local_lr, weight_decay=1e-4)
+
+        for epoch in range(local_epoch):
+            for batch_data in dataloader_train:
+                inputs, labels = batch_data[0][:,:,:,:,0].to(device), batch_data[1][:,:,:,:,0].to(device)
+                y_pred = ann(inputs)
+                loss = loss_function(y_pred, labels)
+                optimizer.zero_grad()
+                loss.backward()          
+                optimizer.step(self.nn.control, ann.control) #performing SGD on the control variables
+                            
+        # update c
+        # c+ <- ci - c + 1/(E * lr) * (x-yi)
+        temp = {}
+        for k, v in ann.named_parameters():
+            temp[k] = v.data.clone()
+        for k, v in x.named_parameters():
+            ann.control[k] = ann.control[k] - self.nn.control[k] + (v.data - temp[k]) / (local_epoch * local_lr)
+            ann.delta_y[k] = temp[k] - v.data
+            ann.delta_control[k] = ann.control[k] - x.control[k]
+        return ann, loss.item()
+
+#optimizer
+class ScaffoldOptimizer(Optimizer):
+    def __init__(self, params, lr, weight_decay):
+        defaults = dict(lr=lr, weight_decay=weight_decay)
+        super(ScaffoldOptimizer, self).__init__(params, defaults)
+
+    def step(self, server_controls, client_controls, closure=None):
+        for group in self.param_groups:
+            for p, c, ci in zip(group['params'], server_controls.values(), client_controls.values()):
+                if p.grad is None:
+                    continue
+                dp = p.grad.data + c.data - ci.data
+                #local learning rate
+                p.data = p.data - dp.data * group['lr']
---
+++ preprocessing.py
@@ -0,0 +1,162 @@
+from glob import glob
+import torch
+from monai.data import (
+    ArrayDataset,
+    GridPatchDataset,
+    create_test_image_3d,
+    PatchIter)
+from monai.transforms import (
+    Activations,
+    AsChannelFirstD,
+    AddChannel,
+    AsDiscrete,
+    Compose,
+    LoadImage,
+    RandRotate90,
+    RandSpatialCrop,
+    ScaleIntensity,
+    EnsureType,
+    Resized)
+
+def get_train_valid_test_partitions(path, modality, num_centers=4):
+    centers_partitions = [[] for i in range(num_centers)]
+    for center_num in range(1,num_centers+1):
+        center_paths_train  = sorted(glob(path+'center'+str(center_num)+'/train'+'/**/*'+modality+'*/*.nii'))
+        center_paths_valid  = sorted(glob(path+'center'+str(center_num)+'/valid'+'/**/*'+modality+'*/*.nii'))
+        center_paths_test   = sorted(glob(path+'center'+str(center_num)+'/test'+'/**/*'+modality+'*/*.nii'))
+        center_lbl_paths_train  = sorted(glob(path+'center'+str(center_num)+'/train'+'/**/*OT*/*nii'))
+        center_lbl_paths_valid  = sorted(glob(path+'center'+str(center_num)+'/valid'+'/**/*OT*/*nii'))
+        center_lbl_paths_test   = sorted(glob(path+'center'+str(center_num)+'/test'+'/**/*OT*/*nii'))
+        centers_partitions[center_num-1] = [[center_paths_train,center_paths_valid,center_paths_test],[center_lbl_paths_train,center_lbl_paths_valid,center_lbl_paths_test]]
+    return centers_partitions
+
+def center_dataloaders(partitions_paths_center, transfo, batch_size=2):
+    center_ds_train = ArrayDataset(partitions_paths_center[0][0], transfo['imtrans'], partitions_paths_center[1][0], transfo['segtrans'])
+    center_train_loader = torch.utils.data.DataLoader(
+    	center_ds_train, batch_size=batch_size, num_workers=1, pin_memory=torch.cuda.is_available()
+    	)
+
+    center_ds_valid = ArrayDataset(partitions_paths_center[0][1], transfo['imtrans'], partitions_paths_center[1][1], transfo['segtrans'])
+    center_valid_loader = torch.utils.data.DataLoader(
+    	center_ds_valid, batch_size=batch_size, num_workers=1, pin_memory=torch.cuda.is_available()
+    	)
+
+    center_ds_test = ArrayDataset(partitions_paths_center[0][2], transfo['imtrans_test'], partitions_paths_center[1][2], transfo['segtrans_test'])
+    center_test_loader = torch.utils.data.DataLoader(
+        center_ds_test, batch_size=batch_size, num_workers=1, pin_memory=torch.cuda.is_available()
+    	)
+    return center_train_loader, center_valid_loader, center_test_loader
+
+def dataPreprocessing(path, modality, number_site, batch_size):
+	#creating the dataloader for 10 ISLES volumes using the T_max and the CBF
+    #For cbf we are windowing 1-1024
+    #For tmax we'll window 0-60
+    #For CBV we'll window 0-200
+    max_intensity=30
+    if modality =='CBF':
+        max_intensity = 1200
+    elif modality =='CBV':
+        max_intensity = 200
+    elif modality =='Tmax' or modality =='MTT':
+        max_intensity = 30
+
+    transfo = {}
+
+    imtrans = Compose(
+        [   LoadImage(image_only=True),
+            #RandScaleIntensity( factors=0.1, prob=0.5),
+            ScaleIntensity(minv=0.0, maxv=max_intensity),
+            AddChannel(),
+            RandRotate90( prob=0.5, spatial_axes=[0, 1]),
+            RandSpatialCrop((224, 224,1), random_size=False),
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['imtrans']=imtrans
+
+    segtrans = Compose(
+        [   LoadImage(image_only=True),
+            AddChannel(),
+            RandRotate90( prob=0.5, spatial_axes=[0, 1]),
+            RandSpatialCrop((224, 224,1), random_size=False),
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['segtrans']=segtrans
+
+
+
+    imtrans_neutral = Compose(
+        [   LoadImage(image_only=True),
+            #RandScaleIntensity( factors=0.1, prob=0.5),
+            ScaleIntensity(minv=0.0, maxv=max_intensity),
+            AddChannel(),
+            RandSpatialCrop((224, 224,1), random_size=False),
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['imtrans_neutral']=imtrans_neutral
+
+    segtrans_neutral = Compose(
+        [   LoadImage(image_only=True),
+            AddChannel(),
+            RandSpatialCrop((224, 224,1), random_size=False),
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['segtrans_neutral']=segtrans_neutral
+
+    imtrans_test = Compose(
+        [   LoadImage(image_only=True),
+            ScaleIntensity(minv=0.0, maxv=max_intensity),
+            AddChannel(),
+            #RandSpatialCrop((224, 224,1), random_size=False), In test we would like to process ALL slices
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['imtrans_test']=imtrans_test
+
+    segtrans_test = Compose(
+        [   LoadImage(image_only=True),
+            AddChannel(),
+            #RandSpatialCrop((224, 224,1), random_size=False),
+            EnsureType(),
+            #Resized
+        ]
+    )
+    transfo['segtrans_test']=segtrans_test
+
+    partitions_paths = get_train_valid_test_partitions(path, modality, number_site)
+    
+    centers_data_loaders = []
+    for i in range(len(partitions_paths)):#Adding all the centers data loaders
+        centers_data_loaders.append(center_dataloaders(partitions_paths[i], transfo, batch_size))
+
+    partitions_test_imgs = [partitions_paths[i][0][2] for i in range(len(partitions_paths))]
+    partitions_test_lbls = [partitions_paths[i][1][2] for i in range(len(partitions_paths))]
+
+    partitions_valid_imgs = [partitions_paths[i][0][1] for i in range(len(partitions_paths))]
+    partitions_valid_lbls = [partitions_paths[i][1][1] for i in range(len(partitions_paths))]
+
+    #For selecting the model and testing in the heldout partition we collect the valid and test data from ALL centers
+    all_ds_test = ArrayDataset([i for l in partitions_test_imgs for i in l],
+                                transfo['imtrans'], [i for l in partitions_test_lbls for i in l],
+                                transfo['segtrans'])
+    all_test_loader   = torch.utils.data.DataLoader(
+        all_ds_test, batch_size=1, num_workers=1, pin_memory=torch.cuda.is_available()
+    )
+
+
+    all_ds_valid = ArrayDataset([i for l in partitions_valid_imgs for i in l],
+                                transfo['imtrans'], [i for l in partitions_valid_lbls for i in l],
+                                transfo['segtrans'])
+    all_valid_loader   = torch.utils.data.DataLoader(
+        all_ds_valid, batch_size=1, num_workers=1, pin_memory=torch.cuda.is_available()
+    )
+
+    return partitions_paths, centers_data_loaders, all_test_loader, all_valid_loader
