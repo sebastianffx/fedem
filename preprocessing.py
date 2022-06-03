6ddc58cfb28d4a85c46312a03a781a52c830b109
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-03 18:53:36 +0200

    restauring the original loaders

--- preprocessing.py
+++ preprocessing.py
@@ -30,7 +30,8 @@ def get_train_valid_test_partitions(path, modality, num_centers=4, nested=True):
             center_lbl_paths_test   = sorted(glob(path+'center'+str(center_num)+'/test'+'/**/*OT*/*nii'))
             centers_partitions[center_num-1] = [[center_paths_train,center_paths_valid,center_paths_test],[center_lbl_paths_train,center_lbl_paths_valid,center_lbl_paths_test]]
             print("site", str(center_num), "data loader contains (train/valid/test) map", len(center_paths_train), len(center_paths_valid), len(center_paths_test))
-            print("site", str(center_num), "data loader contains (train/valid/test) label", len(center_lbl_paths_train), len(center_lbl_paths_valid), len(center_lbl_paths_test))
+            if (len(center_paths_train), len(center_paths_valid), len(center_paths_test)) != (len(center_lbl_paths_train), len(center_lbl_paths_valid), len(center_lbl_paths_test)):
+                print("not same number of images and masks!")
     else:
         for center_num in range(1,num_centers+1):
             center_paths_train  = sorted(glob(path+'center'+str(center_num)+'/train/'+f'*{modality.lower()}.nii*'))
@@ -41,7 +42,8 @@ def get_train_valid_test_partitions(path, modality, num_centers=4, nested=True):
             center_lbl_paths_test   = sorted(glob(path+'center'+str(center_num)+'/test/' +'*msk.nii*'))
             centers_partitions[center_num-1] = [[center_paths_train,center_paths_valid,center_paths_test],[center_lbl_paths_train,center_lbl_paths_valid,center_lbl_paths_test]]
             print("site", str(center_num), "data loader contains (train/valid/test) map", len(center_paths_train), len(center_paths_valid), len(center_paths_test))
-            print("site", str(center_num), "data loader contains (train/valid/test) label", len(center_lbl_paths_train), len(center_lbl_paths_valid), len(center_lbl_paths_test))
+            if (len(center_paths_train), len(center_paths_valid), len(center_paths_test)) != (len(center_lbl_paths_train), len(center_lbl_paths_valid), len(center_lbl_paths_test)):
+                print("not same number of images and masks!")
     return centers_partitions
 
 def center_dataloaders(partitions_paths_center, transfo, batch_size=2):
@@ -52,7 +54,7 @@ def center_dataloaders(partitions_paths_center, transfo, batch_size=2):
     #print(len(partitions_paths_center[1][1]))
     #print(len(partitions_paths_center[0][2]))
     #print(len(partitions_paths_center[1][2]))
-    """
+    
     center_ds_train = ArrayDataset(partitions_paths_center[0][0], transfo['imtrans'], partitions_paths_center[1][0], transfo['segtrans'])
     center_train_loader = torch.utils.data.DataLoader(
     	center_ds_train, batch_size=batch_size, num_workers=1, pin_memory=torch.cuda.is_available()
@@ -82,6 +84,7 @@ def center_dataloaders(partitions_paths_center, transfo, batch_size=2):
     center_test_loader = torch.utils.data.DataLoader(
         center_ds_test, batch_size=batch_size, num_workers=0, pin_memory=torch.cuda.is_available()
     	)
+    """
     #print("===========LEN LOADERS==========")
     #print(len(center_train_loader))
     #print(len(center_valid_loader))
@@ -199,21 +202,23 @@ def dataPreprocessing(path, modality, number_site, batch_size, size_crop=224, ne
     partitions_valid_lbls = [partitions_paths[i][1][1] for i in range(len(partitions_paths))]
 
     #For selecting the model and testing in the heldout partition we collect the valid and test data from ALL centers
-    #all_ds_test = ArrayDataset([i for l in partitions_test_imgs for i in l],
-    #                            transfo['imtrans'], [i for l in partitions_test_lbls for i in l],
-    #                            transfo['segtrans'])
+    all_ds_test = ArrayDataset([i for l in partitions_test_imgs for i in l], transfo['imtrans'],
+                               [i for l in partitions_test_lbls for i in l], transfo['segtrans'])
+    """
     all_ds_test = ArrayDataset([i for l in partitions_test_imgs for i in l], transfo['debug'],
                                [i for l in partitions_test_lbls for i in l], transfo['debug'])
+    """
     all_test_loader   = torch.utils.data.DataLoader(
         all_ds_test, batch_size=1, num_workers=0, pin_memory=torch.cuda.is_available()
     )
 
 
-    #all_ds_valid = ArrayDataset([i for l in partitions_valid_imgs for i in l],
-    #                            transfo['imtrans'], [i for l in partitions_valid_lbls for i in l],
-    #                            transfo['segtrans'])
+    all_ds_valid = ArrayDataset([i for l in partitions_valid_imgs for i in l], transfo['imtrans'],
+                                [i for l in partitions_valid_lbls for i in l], transfo['segtrans'])
+    """
     all_ds_valid = ArrayDataset([i for l in partitions_valid_imgs for i in l], transfo['debug'],
                                 [i for l in partitions_valid_lbls for i in l], transfo['debug'])
+    """
     all_valid_loader   = torch.utils.data.DataLoader(
         all_ds_valid, batch_size=1, num_workers=0, pin_memory=torch.cuda.is_available()
     )
