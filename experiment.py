c0c43c7b95363d4bc6abaca31ce7945e65015028
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-08 12:11:28 +0200

    tweaking the leanring rate

--- experiment.py
+++ experiment.py
@@ -130,16 +130,16 @@ if __name__ == '__main__':
     check_dataset(path, number_site, dim=(144,144,42))
 
     centralized = default.copy()
-    centralized.update({"centralized":True, "l_lr":1e-3})
+    centralized.update({"centralized":True, "l_lr":1e-2})
 
     centralized2 = centralized.copy()
-    centralized.update({"l_lr":5e-4})
+    centralized.update({"l_lr":5e-2})
 
     centralized3 = centralized.copy()
-    centralized.update({"l_lr":1e-4})
+    centralized.update({"l_lr":1e-3})
 
     centralized4 = centralized.copy()
-    centralized.update({"l_lr":1e-5})
+    centralized.update({"l_lr":1e-4})
 
     fedrod = default.copy()
     fedrod.update({"fedrod":True})
