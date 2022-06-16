936c80c921b502247e47d936e0d58ed8294d8128
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-16 14:16:46 +0200

    more robust to missing files

--- debug_util.py
+++ debug_util.py
@@ -21,14 +21,20 @@ def replace_masks(path_to_dataset, path_mask_archive):
     """
     #TODO: also copy the labels with component labeling, could be useful for the blob loss
 
+    count_missing = 0
     for center in os.listdir(path_to_dataset):
         print("processing", str(center))
         for split in ["train", "valid", "test"]:
             #extract te subjects using the mask file
             split_subjects = [f for f in os.listdir(os.path.join(path_to_dataset, center, split)) if "msk." in f]
             for subject in split_subjects:
-                #overwrite the existing mask with the new mask
-                shutil.copy(os.path.join(path_mask_archive, subject.replace("_msk.", "_ANTS_HDBET_FAST_smooth_concomp_defuseSeg_labeled.")),
-                            os.path.join(os.path.join(path_to_dataset, center, split, subject)))
+                source_file = os.path.join(path_mask_archive, subject.replace("_msk.", "_ANTS_HDBET_FAST_smooth_concomp_defuseSeg_labeled."))
+                if os.path.exists(source_file):
+                    #overwrite the existing mask with the new mask
+                    shutil.copy(source_file,
+                                os.path.join(os.path.join(path_to_dataset, center, split, subject)))
+                else:
+                    count_missing+=1
+                    print("new label does not exist for", subject, "keeping the old labels!")
 
-                print("processed", subject)
+    print(count_missing, "subjects could not have updates with the new labels")
