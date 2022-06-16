b489616bc6ed87e2017cd47988aae9a83752205c
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-16 13:50:03 +0200

    adding function to copy new labels to an existing dataset

--- debug_util.py
+++ debug_util.py
@@ -1,7 +1,11 @@
+import os
+import shutil
 import numpy as np
 import nibabel as nb
 
 def crop2seg(path, r_low, r_high):
+    """ Crop volume and the corresponding labels to a given slices range
+    """
     img = nb.load(path)
     img_affine = img.affine
 
@@ -11,3 +15,15 @@ def crop2seg(path, r_low, r_high):
     img = nb.load(path.replace("adc.", "msk."))
     img = img.get_fdata()[:,:,r_low:r_high]
     nb.save(nb.Nifti1Image(img, img_affine), path.replace("adc.", "msk."))
+
+def replace_masks(path_to_dataset, path_mask_archive):
+    """ Replace one dataset mask with new masks, using the name of the subject
+    """
+    for center in os.listdir(path_to_dataset):
+        for split in ["train", "valid", "test"]:
+            #extract te subjects using the mask file
+            split_subjects = [f for f in s.listdir(os.path.join(path_to_dataset, split)) if "msk." in f]
+            for subject in split_subjects:
+                #overwrite the existing mask with the new mask
+                shutil.copy(os.path.join(path_mask_archive, f.replace("_msk.", "_ANTS_HDBET_FAST_smooth_concomp_defuseSeg_labeled.")),
+                            os.path.join(os.path.join(path_to_dataset, split, f)))
