f6a44307a3651fe7714a04ca1d57b2a09ba6135c
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-16 14:03:56 +0200

    printing progress

--- debug_util.py
+++ debug_util.py
@@ -30,3 +30,5 @@ def replace_masks(path_to_dataset, path_mask_archive):
                 #overwrite the existing mask with the new mask
                 shutil.copy(os.path.join(path_mask_archive, subject.replace("_msk.", "_ANTS_HDBET_FAST_smooth_concomp_defuseSeg_labeled.")),
                             os.path.join(os.path.join(path_to_dataset, center, split, subject)))
+
+                print("processed", subject)
