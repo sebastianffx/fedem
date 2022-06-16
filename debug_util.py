8b1857aedb246abc9cbe106616c3a955576a0e42
Antoine Madrona <antoine.madrona@epfl.ch>
2022-06-16 13:59:01 +0200

    forgot one folder level

--- debug_util.py
+++ debug_util.py
@@ -25,7 +25,7 @@ def replace_masks(path_to_dataset, path_mask_archive):
         print("processing", str(center))
         for split in ["train", "valid", "test"]:
             #extract te subjects using the mask file
-            split_subjects = [f for f in os.listdir(os.path.join(path_to_dataset, split)) if "msk." in f]
+            split_subjects = [f for f in os.listdir(os.path.join(path_to_dataset, center, split)) if "msk." in f]
             for subject in split_subjects:
                 #overwrite the existing mask with the new mask
                 shutil.copy(os.path.join(path_mask_archive, f.replace("_msk.", "_ANTS_HDBET_FAST_smooth_concomp_defuseSeg_labeled.")),
